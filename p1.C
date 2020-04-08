eaa042c662ba981a7554db50f16b543337ee4e5f
J Clement <jnc8mc@virginia.edu>
2020-04-08 05:00:38 -0400

    made box larger

--- p1.C
+++ p1.C
@@ -16,9 +16,9 @@ using std::vector;
 using std::cout;
 using std::endl;
 
-const double L = 100;   // Length of any side
+const int L = 100;   // Length of any side
 const double V0 = 100;  // Voltage at top of box
-const double dist = 20; // distance from plate to wall, should be < Npts / 2 for best results 
+const int dist = 45; // distance from plate to wall, should be < Npts / 2 for best results 
 const int maxgraphlines = 200;  // Max lines to draw in each direction
 
 // Generic code to do one iteration of finite difference method
@@ -42,13 +42,13 @@ double iterateJ(vector<vector<double>> &V) {
 
 
 // Gauss-Seidel Method
-double iterateGS(vector<vector<double>> &V) {
+double iterateGS(vector<vector<double>> &V, int Npts) {
   double dVmax = 1e-50;
   int nx = V.size();
   int ny = V[0].size();
   for(int i = 1; i < nx - 1; i++) {
     for(int j = 1; j < ny - 1; j++) {
-      if(j != dist && j != ny - 1 - dist) {  // Preserve potential at plate
+      if(!((j == dist || j == ny - 1 - dist) && (i < L/2+Npts/2 || i > L/2-Npts/2))) {  // Preserve potential at plate
 	double Vnew = 0.25 * (V[i + 1][j] + V[i - 1][j] +
 			      V[i][j + 1] + V[i][j - 1]);
 	double dV = fabs(Vnew - V[i][j]);
@@ -87,12 +87,12 @@ void fillGraph(TGraph2D* tg, const vector<vector<double>> &V, double delta,
 TGraph2D* LaplaceLine(int maxIter = 100, double eps = 0.001, int Npts = 100,
 		      TCanvas *tc = 0, int rate = 10) {
    // create N x N vector, init to 
-  vector<vector<double>> V(Npts, vector<double> (Npts, 0));
-  double delta = L / (Npts - 1);  // Grid spacing
+  vector<vector<double>> V(L, vector<double> (L, 0));
+  double delta = 1;  // Grid spacing
 
   
-  for(int i = 0; i < Npts; i++) {
-    V[i][Npts - 1 - dist] = -V0;  // Set voltage at plates
+  for(int i = L/2-Npts/2; i < L/2+Npts/2; i++) {
+    V[i][L - 1 - dist] = -V0;  // Set voltage at plates
     V[i][dist]        =  V0;
   }
   
@@ -101,19 +101,18 @@ TGraph2D* LaplaceLine(int maxIter = 100, double eps = 0.001, int Npts = 100,
   int msec = 1000 / rate;   // Milliseconds sleep between frames
   TBox *plotRange = new TBox(0, 0, 1.1 * L, 1.1 * L);
 
-  TGraph2D* tgV = new TGraph2D();  // Graph to store result
-  if(Npts < 50) tgV->SetLineWidth(3);                         
+  TGraph2D* tgV = new TGraph2D();  // Graph to store result               
   tgV->SetLineColor(kRed);
-  tgV->SetNpx(std::min(maxgraphlines, Npts));
-  tgV->SetNpy(std::min(maxgraphlines, Npts)); 
+  tgV->SetNpx(std::min(maxgraphlines, L));
+  tgV->SetNpy(std::min(maxgraphlines, L)); 
   tgV->SetTitle("Voltage;x;y;V");
   
   double dV;
   int niter = 0;
-  
+
   do {
     //dV = iterateJ(V);   // iterate using Jacobi method
-    dV = iterateGS(V);   // iterate using Gauss-Seidel method
+    dV = iterateGS(V, Npts);   // iterate using Gauss-Seidel method
     ++niter;
     if (tc) {
       tc->cd();
@@ -154,7 +153,7 @@ int main(int argc, char *argv[]) {
   // Defaults for LaplaceLine
   int maxIter = 100;
   double eps  = 0.001;
-  int Npts    = 100;
+  int Npts    = 20;
   TCanvas *tc = 0;
   int rate    = 10;
   
